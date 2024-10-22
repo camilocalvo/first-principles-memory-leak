ba1e0d82354cb98481b57302689cb6c2cae8ca7d
camilo.calvoalcaniz <camilo.calvoalcaniz@zoll.com>
2024-10-22 17:11:26 -0400

    fpml: Added Ionic support and some Ionic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { IonButton, IonText, setupIonicReact } from "@ionic/react";
+import "@ionic/react/css/core.css";
+
+setupIonicReact();
 
 function App() {
   return (
@@ -10,6 +14,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <IonText>Ion text</IonText>
+        <IonButton>Ion button</IonButton>
         <a
           className="App-link"
           href="https://reactjs.org"
